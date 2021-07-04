724f2c30ff1a50e4fded73ee5b162dbb386e98a6
asdia0 <asdia.the.potato@gmail.com>
2021-07-05 00:44:31 +0800

    Rename MainWindow to Main

--- src/EloCalculator.GUI/Main.xaml.cs
+++ src/EloCalculator.GUI/Main.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace EloCalculator.GUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Main.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Main : Window
     {
-        public MainWindow()
+        public Main()
         {
             InitializeComponent();
         }
