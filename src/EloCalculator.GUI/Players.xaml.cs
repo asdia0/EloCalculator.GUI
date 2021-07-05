4d7143ae466ec68563013912e8d6cc83a2e2f740
asdia0 <asdia.the.potato@gmail.com>
2021-07-05 15:14:32 +0800

    Add Players

---
+++ src/EloCalculator.GUI/Players.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace EloCalculator.GUI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for Players.xaml.
+    /// </summary>
+    public partial class Players : Window
+    {
+        public Players()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
