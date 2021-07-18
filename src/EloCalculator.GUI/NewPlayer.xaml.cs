b7890b665102632d26a12f7a85f7205dd77c0c18
asdia0 <asdia.the.potato@gmail.com>
2021-07-19 00:29:47 +0800

    Add NewPlayer

---
+++ src/EloCalculator.GUI/NewPlayer.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace EloCalculator.GUI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using EloCalculator;
+
+    /// <summary>
+    /// Interaction logic for NewPlayer.xaml
+    /// </summary>
+    public partial class NewPlayer : Window
+    {
+        public NewPlayer()
+        {
+            this.InitializeComponent();
+        }
+
+        public void Add_OnClick(object sender, RoutedEventArgs e)
+        {
+            Player p = new(this.Name.Text);
+            Utility.UpdateGames();
+            Utility.UpdatePlayers();
+            this.Close();
+        }
+
+        public void Cancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
