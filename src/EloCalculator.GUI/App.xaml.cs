6f7a5e1b86e4f5ff624be7d1c67e06d1fb6402d1
asdia0 <asdia.the.potato@gmail.com>
2021-07-18 23:42:03 +0800

    Add Application_OnExit()

--- src/EloCalculator.GUI/App.xaml.cs
+++ src/EloCalculator.GUI/App.xaml.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace EloCalculator.GUI
+﻿namespace EloCalculator.GUI
 {
+    using System.Windows;
+    using EloCalculator;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private void Application_OnExit(object sender, ExitEventArgs e)
+        {
+            GameDatabase.Export("EloCalculator.GUI/Games.json");
+            PlayerDatabase.Export("EloCalculator.GUI/Players.json");
+            TournamentDatabase.Export("EloCalculator.GUI/Tournaments.json");
+        }
     }
 }
