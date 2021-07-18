4a57bb817443b5a9b1ed020736a879ac32a37407
asdia0 <asdia.the.potato@gmail.com>
2021-07-18 23:20:30 +0800

    Add Utility

---
+++ src/EloCalculator.GUI/Utility.cs
@@ -0,0 +1,10 @@
+﻿namespace EloCalculator.GUI
+{
+    using System.Collections.ObjectModel;
+    using System.Collections.Generic;
+
+    public static class Utility
+    {
+        public static ObservableCollection<Game> Games = new();
+    }
+}
