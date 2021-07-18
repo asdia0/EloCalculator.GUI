7352a95243692c7cb0ddb7a6da8b4c512d5b7457
asdia0 <asdia.the.potato@gmail.com>
2021-07-18 23:22:25 +0800

    Add NewGame window

--- src/EloCalculator.GUI/NewGame.xaml.cs
+++ src/EloCalculator.GUI/NewGame.xaml.cs
@@ -3,16 +3,8 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Windows;
-    using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Shapes;
+    using EloCalculator;
 
     /// <summary>
     /// Interaction logic for NewGame.xaml
@@ -22,6 +14,33 @@
         public NewGame()
         {
             this.InitializeComponent();
+            this.WhiteID.ItemsSource = PlayerDatabase.Players.Select(i => $"{i.ID} ({i.Name})");
+            this.BlackID.ItemsSource = PlayerDatabase.Players.Select(i => $"{i.ID} ({i.Name})");
+            this.Result.ItemsSource = new List<string>()
+            {
+                "White",
+                "Black",
+                "Draw",
+            };
+        }
+
+        public void Add_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (this.WhiteID.SelectedIndex == this.BlackID.SelectedIndex)
+            {
+                // create new window
+            }
+            else
+            {
+                Game g = new Game(PlayerDatabase.Players[this.WhiteID.SelectedIndex], PlayerDatabase.Players[this.BlackID.SelectedIndex], (Result)int.Parse(this.Result.SelectedIndex.ToString()), DateTime.Now, (bool)this.Rated.IsChecked);
+                Utility.Games.Add(g);
+            }
+            this.Close();
+        }
+
+        public void Cancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
