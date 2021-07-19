c848fa2403f027342a14e0e69551e521c469c0c7
asdia0 <asdia.the.potato@gmail.com>
2021-07-19 19:01:52 +0800

    Add NewTournamentRound

---
+++ src/EloCalculator.GUI/NewTournamentRound.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace EloCalculator.GUI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using EloCalculator;
+
+    /// <summary>
+    /// Interaction logic for NewGame.xaml
+    /// </summary>
+    public partial class NewTournamentRound : Window
+    {
+        public NewTournamentRound()
+        {
+            this.InitializeComponent();
+            Utility.UpdatePlayers();
+            Utility.UpdateTournaments();
+            this.Tournament.ItemsSource = Utility.Tournaments.Select(i => $"{i.ID} ({i.Name})");
+        }
+
+        public void Add_OnClick(object sender, RoutedEventArgs e)
+        {
+            TournamentRound r = new(Utility.Tournaments[this.Tournament.SelectedIndex]);
+            Utility.UpdateTournaments();
+            this.Close();
+        }
+
+        public void Cancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
