0a94ab00823f82eb71a90566c4bb4d462957ad26
asdia0 <asdia.the.potato@gmail.com>
2021-07-19 19:01:42 +0800

    Add NewTournamentPlayer

---
+++ src/EloCalculator.GUI/NewTournamentPlayer.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace EloCalculator.GUI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using EloCalculator;
+
+    /// <summary>
+    /// Interaction logic for NewGame.xaml
+    /// </summary>
+    public partial class NewTournamentPlayer : Window
+    {
+        public NewTournamentPlayer()
+        {
+            this.InitializeComponent();
+            Utility.UpdatePlayers();
+            Utility.UpdateTournaments();
+            this.Tournament.ItemsSource = Utility.Tournaments.Select(i => $"{i.ID} ({i.Name})");
+            this.Player.ItemsSource = Utility.Players.Select(i => $"{i.ID} ({i.Name})");
+        }
+
+        public void Add_OnClick(object sender, RoutedEventArgs e)
+        {
+            TournamentPlayer tp = new(Utility.Tournaments[this.Tournament.SelectedIndex], Utility.Players[this.Player.SelectedIndex]);
+            Utility.UpdateTournaments();
+            this.Close();
+        }
+
+        public void Cancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
