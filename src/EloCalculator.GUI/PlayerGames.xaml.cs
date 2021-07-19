2440bda16cf32d4ef1342ebdf24639ecc65582a4
asdia0 <asdia.the.potato@gmail.com>
2021-07-19 16:20:04 +0800

    Add PlayerGames

---
+++ src/EloCalculator.GUI/PlayerGames.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace EloCalculator.GUI
+{
+    using System;
+    using System.Data;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+    using System.Collections.ObjectModel;
+
+    /// <summary>
+    /// Interaction logic for Games.xaml.
+    /// </summary>
+    public partial class PlayerGames : Window
+    {
+        public PlayerGames(Player player)
+        {
+            this.InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+            Utility.UpdateGames();
+            this.DataGrid.ItemsSource = Utility.Games.Where(i => i.WhitePlayer == player || i.BlackPlayer == player);
+            this.DataGrid.IsSynchronizedWithCurrentItem = true;
+        }
+
+        public void PreviewKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            DataGrid grid = (DataGrid)sender;
+            if (e.Key == Key.Delete)
+            {
+                foreach (Game game in grid.SelectedItems)
+                {
+                    GameDatabase.Games.Remove(game);
+                }
+            }
+
+            // Update player stats
+
+            // Update tournament stats
+
+            Utility.UpdateGames();
+            Utility.UpdatePlayers();
+            Utility.UpdateTournaments();
+        }
+    }
+}
