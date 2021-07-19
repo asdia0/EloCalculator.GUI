d7cd86b21e1dad1d869ca8401868dbe7f271fec3
asdia0 <asdia.the.potato@gmail.com>
2021-07-19 16:20:30 +0800

    Add Tournament framework

--- src/EloCalculator.GUI/Tournaments.xaml.cs
+++ src/EloCalculator.GUI/Tournaments.xaml.cs
@@ -1,6 +1,7 @@
 ﻿namespace EloCalculator.GUI
 {
     using System;
+    using System.Data;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
@@ -13,15 +14,44 @@
     using System.Windows.Media;
     using System.Windows.Media.Imaging;
     using System.Windows.Shapes;
+    using System.Collections.ObjectModel;
 
     /// <summary>
-    /// Interaction logic for Tournaments.xaml.
+    /// Interaction logic for Games.xaml.
     /// </summary>
     public partial class Tournaments : Window
     {
         public Tournaments()
         {
             this.InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+            Game add = new(new("Player 1"), new("Player 2"), Result.White, DateTime.Now, true);
+
+            Utility.UpdateGames();
+            this.DataGrid.ItemsSource = Utility.Games;
+            this.DataGrid.IsSynchronizedWithCurrentItem = true;
+        }
+
+        public void PreviewKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            DataGrid grid = (DataGrid)sender;
+            if (e.Key == Key.Delete)
+            {
+                foreach (Tournament tournament in grid.SelectedItems)
+                {
+                    TournamentDatabase.Tournaments.Remove(tournament);
+                }
+            }
+        }
+
+        public void ViewPlayers_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void ViewRounds_OnClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
